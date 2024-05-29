f103f0c595084ffbf6dfe8790643fad5fc420791
oudarja <oudarja360@gmail.com>
2024-05-29 23:07:36 +0600

    js tutorial last commit

---
+++ Js tutorial/Operator.js
@@ -0,0 +1,13 @@
+const a=1;
+const b=2;
+
+
+console.log('Sum :',a+b);
+console.log('Sub :',a-b);
+console.log('Mul :',a*b);
+console.log('Div :',a/b);
+console.log('Mod :',a%b);
+console.log('Pow :',a**b);
+
+
+
